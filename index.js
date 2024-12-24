df5a347bb08ef01783500f516650f648f265b564
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-12-25 03:54:27 +0600

    added GET API endpoint to fetch marathon details by ID

--- index.js
+++ index.js
@@ -56,7 +56,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-
+        // 5.GET (for id wise details)
+        app.get('/marathons/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await marathonCollection.findOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
